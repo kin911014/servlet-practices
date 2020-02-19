78bfe5e7dd7a1bf7c9fa601fb2a3399d796fa7bd
dongeun <kin911014@daum.net>
2020-02-19 17:44:48 +0900

    modify routes

--- helloweb/src/main/java/com/douzone/helloweb/controler/JoinServlet.java
+++ helloweb/src/main/java/com/douzone/helloweb/controler/JoinServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/join")
+@WebServlet("/")
 public class JoinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
